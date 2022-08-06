9553df6134349f929830c658eb489d85e84ebfa8
Septian Dwi Prasetio <110075842+septiandwiprasetio@users.noreply.github.com>
2022-08-07 02:33:19 +0700

    Add files via upload

---
+++ ruby_study_3/page3/index.rb
@@ -0,0 +1,7 @@
+def introduce(name)
+  puts "Halo"
+  puts "Nama saya adalah #{name}"
+end
+
+# Panggil method `introduce` dengan nama Anda sendiri
+introduce("Septian Dwi Prasetio")
---
+++ ruby_study_5/page11/index.rb
@@ -0,0 +1,11 @@
+# Lakukan `import` class `Date` menggunakan `require`
+require "date"
+
+# Tetapkan sebuah instance dari Date pada variable `birthday`
+birthday = Date.new(1992, 9, 17)
+
+# Cetak variable `birthday`
+puts birthday
+
+# Cetak nilai `return` dari method instance `sunday?` dari variable `birthday`
+puts birthday.sunday?
